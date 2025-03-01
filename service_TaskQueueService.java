b8237a43261e0ae39dfd5f9ed2c82ae20e778f77
jstjosh <joshwajoe345@gmail.com>
2025-02-28 23:22:49 -0600

    Update service_TaskQueueService.java

--- service_TaskQueueService.java
+++ service_TaskQueueService.java
@@ -7,15 +7,18 @@ import java.util.Queue;
 public class TaskQueueService {
     private Queue<Task> queue = new LinkedList<>();
 
+    // Enqueue a task
     public void enqueue(Task task) {
         queue.add(task);
     }
 
+    // List tasks
     public Queue<Task> listTasks() {
-        return queue;
+        return new LinkedList<>(queue); // Return a copy to avoid modification
     }
 
+    // Dequeue a task
     public Task dequeue() {
-        return queue.poll();
+        return queue.poll(); // poll() returns null if the queue is empty
     }
 }
